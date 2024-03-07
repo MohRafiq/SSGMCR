7f12b6c4ddd69b53d01f30bb1e0db674db0a58aa
MohRafiq <107100467+MohRafiq@users.noreply.github.com>
2024-03-07 09:27:47 +0530

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+var element;
+element = document.getElementsByClassName("navbar").attributes;
+console.log(element);
+
+// var elements = document.getElementsByClassName("navbar");
+// var elementText = elements[0].innerText; // Assuming you want the text of the first element with class "navbar"
+// console.log(elementText);
